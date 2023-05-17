759bb9d38dd1255487918c67e0472419142e3d39
jnuwagaba <jpnuwgaba1@gmail.com>
2023-05-18 00:18:26 +0300

    fourth

--- src/components/ModelContainer.tsx
+++ src/components/ModelContainer.tsx
@@ -1,16 +1,60 @@
 import React, { useState } from 'react'
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
-import { Environment, OrbitControls, PerspectiveCamera, Stars } from "@react-three/drei";
+import { Environment, OrbitControls, PerspectiveCamera, Stars, MapControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
 
 import { Model } from './Model'
+import { CollegeLabels } from './College-labels'
 import TopBar from './TopBar';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import Switch from '@mui/material/Switch';
+
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
 const ModelContainer = () => {
+  const [toggleMenu, setToggleMenu] = useState(true);
+  const [toggleCollegesLayer, setToggleCollegesLayer] = useState(false);
+  const [toggleHallsLayer, setToggleHallsLayer] = useState(false);
+  const [toggleOpenSpacesLayer, settoggleOpenSpacesLayer] = useState(false);
+  const [toggleHangoutSpotsLayer, setToggleHangoutSpotsLayer] = useState(false);
+
+  const Menu = () => {
+    return (
+      <div className='fixed right-6 md:right-10 top-12 bg-white/75 p-4 rounded-md'>
+        <div>
+          <div className="flex justify-between items-center">
+            <div className="font-bold text-lg">Places</div>
+          </div>
+          <div className=" mt-5 flex flex-col gap-3">
+            <div className="rounded border md p-3 w-[200px] cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">
+              <div>Colleges</div>
+              <div onClick={() => setToggleCollegesLayer(!toggleCollegesLayer)}><Switch checked={toggleCollegesLayer} {...label} /></div>
+            </div>
+            <div className="rounded border md p-3 w-[200px] cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">
+              <div>Halls</div>
+              <div onClick={() => setToggleHallsLayer(!toggleHallsLayer)}><Switch checked={toggleHallsLayer} {...label} /></div>
+            </div>
+            <div className="rounded border md p-3 w-[200px] cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">
+              <div>Open Spaces</div>
+              <div onClick={() => settoggleOpenSpacesLayer(!toggleOpenSpacesLayer)}><Switch checked={toggleOpenSpacesLayer} {...label} /></div>
+            </div>
+            <div className="rounded border md p-3 w-[200px] cursor-pointer hover:bg-gray-100 flex flex-row items-center justify-between">
+              <div>Hangout Spots</div>
+              <div onClick={() => setToggleHallsLayer(!toggleHangoutSpotsLayer)}><Switch checked={toggleHangoutSpotsLayer} {...label} /></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className=''>
+    <div className='w-full h-screen'>
       <Canvas
         camera={{ position: [0, 50, 275], fov: 30 }}
         style={{ height: '100vh', width: '100%' }}
@@ -18,15 +62,42 @@ const ModelContainer = () => {
         <PerspectiveCamera fov={50} near={0.1} far={100000} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[0, 10, 5]} intensity={0.5} />
-        <OrbitControls autoRotate={true} autoRotateSpeed={0.2}/>
+        <MapControls
+          enableDamping
+          dampingFactor={0.1}
+          maxPolarAngle={Math.PI / 3}
+          autoRotate={true} autoRotateSpeed={0.2}
+          target={[0, 0, 0]} // Camera points at the origin
+          minDistance={50} // Set minimum zoom level
+          maxDistance={270} // Set maximum zoom level
+        />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+        />
         <Suspense fallback={null}>
           <mesh position={[-400, 0, 400]} rotation={[0, 0, 0]}>
             <Model />
+            <mesh position={toggleCollegesLayer ? [0, 0, 0] : [0, 200, 0]}><CollegeLabels /></mesh>
           </mesh>
         </Suspense>
-        <gridHelper args={[20, 20, 0xff0000, 'teal']} />
+        {/* <gridHelper args={[20, 20, 0xff0000, 'teal']} /> */}
       </Canvas>
+
+      <Canvas>
+        <pointLight position={[10, 10, 10]} />
+        <mesh position={toggleCollegesLayer ? [0, 0, 0] : [0, 2, 0]}>
+          <sphereGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
+
       <TopBar />
+      <div onClick={() => setToggleMenu(!toggleMenu)} className='fixed right-6 md:right-10 top-5 cursor-pointer'>
+        {toggleMenu ? <FontAwesomeIcon icon={faChevronLeft} size="xl" /> : <FontAwesomeIcon icon={faChevronRight} size="xl" />}
+      </div>
+      <div style={{ display: toggleMenu ? 'none' : 'block' }}><Menu /></div>
     </div>
   )
 }
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -12,15 +12,15 @@ const TopBar = () => {
   const [threeD, setThreeD] = useState(true);
   return (
     <div className="fixed top-0 left-0 w-full">
-      <div className="flex flex-row justify-between w-full px-12 py-5">
+      <div className="flex flex-row justify-between w-full px-6 md:px-12 py-5">
         <div className='font-bold lg:text-lg'>Makerere Smart Campus</div>
         <div className="flex flex-row items-center gap-12">
-          <div className='cursor-pointer' onClick={() => setThreeD(!threeD)}>
+          {/* <div className='cursor-pointer' onClick={() => setThreeD(!threeD)}>
             {threeD ? <FontAwesomeIcon icon={faCube} size='xl' /> : <FontAwesomeIcon icon={faMap} size="xl" />}
           </div>
           <div className='cursor-pointer' onClick={() => setShowmenu(!showMenu)}>
             {showMenu ? <FontAwesomeIcon icon={faBars} size="xl" /> : <FontAwesomeIcon icon={faCircleXmark} size="xl" />}
-          </div>
+          </div> */}
         </div>
       </div>
       <div style={{ display: showMenu ? 'none' : 'block' }} className=""><PlacesMenu /></div>
